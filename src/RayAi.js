a6678d058f55955832158441bb696f09e547e66c
fluentinstroll <fluentinstroll@protonmail.com>
2021-01-14 11:39:07 -0500

    added help command and changed folder structure

--- src/RayAi.js
+++ src/RayAi.js
@@ -1,15 +1,20 @@
+// Client variables
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const search = require('./commands/search.js');
-// bot token (how to keep this secret?)
-const bot_token = 'Nzk5Mjk1MzI4NjExNTk4Mzc3.YABfyA.HpfUUZrHouviIS_Qn5fBvyfebsA';
+
+// Command variables
+const search = require('./commands/search');
+const help = require('./commands/help');
+
+// bot token TODO: (how to keep this secret?)
+const bot_token = '';
 
 // login to the server with the bot token
 client.login(bot_token);
 
 client.on('ready', () => {
 
-    console.log(`Connected as + ${client.user.tag}`);
+    console.log('Connected as ' + client.user.tag);
 
     // get channel id
     let generalChannel = client.channels.cache.get('772109746126979134');
@@ -17,21 +22,44 @@ client.on('ready', () => {
     generalChannel.send('Hello, world!');
 });
 
-client.on('message', (recievedMessage) => {
+client.on('message', (receivedMessage) => {
     //prevent bot from responding to its own messages
     //there might be a better way
-    if(recievedMessage.author == client.user){
+    if(receivedMessage.author == client.user){
         return;
     }
     
+
     //if the bot command is mentioned...
-    if(recievedMessage.content.startsWith(`ai`)){
-        processCommand(recievedMessage);
+    if(receivedMessage.content.startsWith('ai')){
+        processCommand(receivedMessage);
     }
 })
 
-processCommand = (recievedMessage) => {
-    
+processCommand = (receivedMessage) => {
+    let fullCommand = receivedMessage.content.substr(2) // Remove the leading 'ai'
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[1] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(2) // All other words are arguments/parameters/options for the command
+
+console.log(fullCommand)
+console.log(splitCommand)
+console.log(primaryCommand)
+console.log(arguments)
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("I don't understand the command. Try `ai help`")
+    }
+}
+
+helpCommand = (arguments, receivedMessage) => {
+    let message = help.sendMessage(arguments, receivedMessage);
+    receivedMessage.channel.send(message);
 }
 
 
---
+++ src/commands/help.js
@@ -0,0 +1,9 @@
+
+
+exports.sendMessage = (arguments, receivedMessage) => {
+    if(arguments.length > 0) {
+        return('It looks like you might need help with ' + arguments)
+    } else {
+        return("I'm not sure what you need help with. Try `ai help [topic]`")
+    }
+}
