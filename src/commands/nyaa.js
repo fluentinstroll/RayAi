9c9f8a86ad13c7a465048205443af1156439407e
fluentinstroll <fluentinstroll@protonmail.com>
2021-01-16 13:52:40 -0500

    removed links from message

--- src/commands/nyaa.js
+++ src/commands/nyaa.js
@@ -1,30 +1,39 @@
+const { Message } = require('discord.js');
 const { si } = require('nyaapi');
 
 exports.sendMessage = async (arguments) => {
-    let anime = [];
-    let searchTerm = arguments.toString();
+    let animeNames = []; //store names of anime we search for
+    let animeLinks =[]; //store links to torrents of anime we search for
+    let searchTerm = arguments.toString(); //make sure our arguments are String values
     
     console.log("searchTerm: " + searchTerm + "\n");
 
     await si.search(searchTerm, 10, opts = {filter: 2})
-    .then((data, limit) => {
+    .then((data) => {
         
         data.forEach((data, index) => {
             //console.log("name: " + data.name + " link: " + data.torrent); //testing
-            anime[index] = "name: " + data.name + "\nlink: " + data.torrent + "\n";
+            animeNames[index] = data.name.toString() + "\n";
+            animeLinks[index] = "link: " + data.torrent + "\n";
         });
-        console.log(anime);
+        console.log(animeNames);
     })
     .catch((err) => {
-        console.log('*********REACHED CATCH*********\n');
+        console.log('*********ERROR*****************\n');
         console.log(err);
-        console.log('*******************************');
+        console.log('*******************************\n');
     })
 
-    return(formatMessage(anime));
+    return(formatMessage(animeNames));
 
 }
 
 formatMessage = (anime) => {
-    return("Found some anime for you: \n" + "```" + anime + "```")
+    let message = '';
+    //build a message out of the list of shows
+    anime.forEach((show) => {
+        message += show;
+    });
+    //remove commas somehow
+    return("Found some anime for you: \n" + "```" + message + "```")
 }
